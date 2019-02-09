042f8e80a511ed6f541024aff2488e4de55a2c61
TatriX <tatrics@gmail.com>
2019-02-09 14:31:08 +0100

    feat(fmt): Implement display for program and it's components

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,36 @@
+use std::fmt::{self, Display};
+
+#[derive(Debug)]
 pub struct Program {
     pub statements: Vec<Statement>,
+}
+
+impl Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for stmt in &self.statements {
+            writeln!(f, "{}", stmt)?;
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
 pub enum Statement {
     Let(LetStatement),
     Return(ReturnStatement),
+    Expression(ExpressionStatement),
+}
+
+impl Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::Statement::*;
+
+        match &self {
+            Let(x) => write!(f, "{}", x),
+            Return(x) => write!(f, "{}", x),
+            Expression(x) => write!(f, "{}", x),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -14,9 +39,32 @@ pub struct LetStatement {
     pub value: Expression,
 }
 
+impl Display for LetStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "let {} = {};", self.name.value, &self.value)
+    }
+}
+
 #[derive(Debug)]
 pub struct ReturnStatement {
     pub value: Expression,
+}
+
+impl Display for ReturnStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "return {};", &self.value)
+    }
+}
+
+#[derive(Debug)]
+pub struct ExpressionStatement {
+    pub expression: Expression,
+}
+
+impl Display for ExpressionStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", &self.expression)
+    }
 }
 
 #[derive(Debug)]
@@ -24,7 +72,23 @@ pub enum Expression {
     Identifier(Identifier),
 }
 
+impl Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::Expression::*;
+
+        match &self {
+            Identifier(x) => write!(f, "{}", x),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Identifier {
     pub value: String,
+}
+
+impl Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
 }
